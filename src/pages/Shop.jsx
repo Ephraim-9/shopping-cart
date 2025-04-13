4604709074473a3f396efacefba1c8495e94552c
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-13 22:17:29 +0500

    fetch and render fakestoreApi data

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Shop() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 1. Fetch data from fakestoreapi
+    fetch("https://fakestoreapi.com/products")
+      // 2. Convert to JSON
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      // 3. Set it in state
+      .then((data) => {
+        const cleanData = data.map((item) => {
+          const newItem = {
+            id: item.id,
+            title: item.title.toUpperCase(),
+            image: item.image,
+            price: item.price,
+          };
+          return newItem;
+        });
+        setProducts(cleanData);
+      })
+      .finally(() => setLoading(false));
+    // 4. Handle loading
+  }, []);
+
   return (
     <div>
       <h2>Shop Page</h2>
-      <p>Welcome to the shop. Products will be displayed here.</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {products.map((p) => (
+            <li key={p.id}>{p.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }

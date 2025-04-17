d1d38876f096617831724aefd127a28eb8540ae4
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-17 22:40:21 +0500

    feat: add quantity input field with increment/decrement buttons and Add to Cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductCard.css";
 
 function ProductCard({ title, price, image }) {
+  const [quantity, setQuantity] = useState(1);
+
+  function increment() {
+    setQuantity((prev) => prev + 1);
+  }
+
+  function decrement() {
+    setQuantity((prev) => Math.max(prev - 1, 1));
+  }
+
   return (
     <div className="product-card">
-      <img src={image} alt="{title}" className="product-image" />
+      <img src={image} alt={title} className="product-image" />
       <h4 className="product-title">{title}</h4>
-      <h6 className="product-price">{price}</h6>
+      <h5 className="product-price">$ {price}</h5>
+
+      <button
+        onClick={() => {
+          increment();
+        }}
+      >
+        +
+      </button>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          decrement();
+        }}
+      >
+        -
+      </button>
+
+      <button>Add to Cart</button>
     </div>
   );
 }

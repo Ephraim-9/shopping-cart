2a1291e17d081fb8cf5c44ba1b534baf11fa8ebc
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-22 22:50:28 +0500

    feat: add navbar, setup tailwind and style using tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "../index.css";
+import logo from "../assets/shopping-cart-logo.jpg";
+import cart from "../assets/add-to-cart.svg";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
+      {/* Logo Section */}
+      <div>
+        <img src={logo} alt="logo" className="h-18 rounded-full " />{" "}
+      </div>
+
+      {/* Navigation Links Section */}
+      <div className="space-x-6">
+        <Link to="/" className="text-m hover:text-gray-400">
+          Home
+        </Link>
+        <Link to="/shop" className="text-m hover:text-gray-400">
+          Shop
+        </Link>
+      </div>
+
+      {/* Cart Icon Section */}
+      <div className="flex items-center">
+        <Link to="/cart">
+          <img
+            src={cart}
+            alt="add to cart"
+            className="h-8 w-8 cursor-pointer hover:opacity-50"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;

022e6b3b7808baf9cae3e1809d1c5b32fac4a0da
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-28 22:16:25 +0500

    feat: show cart items quantity on the cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,18 @@ import { Link } from "react-router-dom";
 import "../index.css";
 import logo from "../assets/shopping-cart-logo.jpg";
 import cart from "../assets/add-to-cart.svg";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 function Navbar() {
+  const { cartItems } = useContext(CartContext);
+
+  const totalItems = cartItems.reduce(
+    (sum, item) => sum + (item.quantity || 0),
+    0
+  );
+  console.log("Cart Items Detailed:", JSON.stringify(cartItems, null, 2));
+  console.log("Total Items:", totalItems);
   return (
     <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
       {/* Logo Section */}
@@ -29,6 +39,9 @@ function Navbar() {
             alt="add to cart"
             className="h-8 w-8 cursor-pointer hover:opacity-50"
           />
+          <span className="absolute top-6 right-0 text-black rounded-full w-5 h-5 flex items-center justify-center text-s">
+            {totalItems}
+          </span>
         </Link>
       </div>
     </div>

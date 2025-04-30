0998ed850420bae3f636a17294e4df6cb51b72fb
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-30 09:25:00 +0500

    feat: persist cartItems in localStorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 function CartProvider({ children }) {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCartProducts = localStorage.getItem("cart");
+    return storedCartProducts ? JSON.parse(storedCartProducts) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
   return (
     <CartContext.Provider value={{ cartItems, setCartItems }}>
       {children}

70e42ea62c882155f18006431702d1e3094cf13e
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-05-02 15:16:11 +0500

    refac: use ProductCard for CartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import ProductCard from "../components/ProductCard";
 
 function Cart() {
   const { cartItems } = useContext(CartContext);
@@ -10,21 +11,15 @@ function Cart() {
       {cartItems.length === 0 ? (
         <p>Cart is empty.</p>
       ) : (
-        cartItems.map(({ image, title, price }, index) => (
-          <div
+        cartItems.map((p, index) => (
+          <ProductCard
             key={index}
-            className="w-full max-w-[250px] p-[2%] m-[5%] shadow-md rounded-[14px] flex flex-col items-end transition-transform duration-300 ease-in-out transform hover:scale-105 bg-white"
-          >
-            <img
-              src={image}
-              alt={title}
-              className="w-full aspect-[1/1] object-contain"
-            />
-            <h4 className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[200px] transition-transform duration-300 ease-in-out transform hover:whitespace-normal hover:overflow-visible hover:text-ellipsis text-black">
-              {title}
-            </h4>
-            <h5 className="text-lg font-semibold text-black">${price}</h5>
-          </div>
+            id={p.id}
+            product={p}
+            title={p.title}
+            price={p.price}
+            image={p.image}
+          />
         ))
       )}
     </>
